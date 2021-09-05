bf365c2c9f262e831362dce7fe82c3812311fdd3
felipenathan <felipe.nathan@guiabolso.com.br>
2021-09-04 23:18:35 -0300

    docker-compose

--- src/main/kotlin/br/com/twittercomplainer/scheduler/TwitterScheduler.kt
+++ src/main/kotlin/br/com/twittercomplainer/scheduler/TwitterScheduler.kt
@@ -5,21 +5,32 @@ import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import org.slf4j.LoggerFactory
 
 class TwitterScheduler {
 
     fun schedule() {
 
+        LOGGER.info("Starting schedule")
         Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate({
             val daysWithoutAnswer = LocalDate.now().run {
                 ChronoUnit.DAYS.between(LAST_RESPONSE_OF_UBER, this)
             }
 
-            ClientConfiguration.getClient().postTweet(text(daysWithoutAnswer))
+            val text = buildText(daysWithoutAnswer)
+            LOGGER.info(text)
+
+            try {
+                val response = ClientConfiguration.getClient().postTweet(text)
+                LOGGER.info("Tweet posted, id: ${response.id}")
+            } catch (ex: Exception) {
+                LOGGER.error("Failed to Post a Tweet: $ex")
+            }
+
         }, INITIAL_DELAY, PERIOD, TimeUnit.DAYS)
     }
 
-    private fun text(daysWithoutAnswer: Long) = """
+    private fun buildText(daysWithoutAnswer: Long) = """
         Este é um post automático,
         
         Hoje faz $daysWithoutAnswer dias que o @Uber_Support não resolve o meu problema e a resposta é sempre a mesma:
@@ -34,5 +45,6 @@ class TwitterScheduler {
         const val INITIAL_DELAY = 0L
         const val PERIOD = 1L
         val LAST_RESPONSE_OF_UBER: LocalDate = LocalDate.of(2021, 8, 16)
+        private val LOGGER = LoggerFactory.getLogger(TwitterScheduler::class.java)
     }
 }
